308ef95a252af3c23c5ecf52c45eb1d778188133
Seoul <seoul@Seouls-MacBook-Air.local>
2023-05-05 17:12:27 +0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            IT WORKS !
+            fib VERSION 2 !
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
